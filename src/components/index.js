21123c0f351c61069423c54eecba3fb2bcfb0d79
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-11-19 20:02:05 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+export {
+    CategoryCard
+}
+from "./CategoryCard/CategoryCard";
+export {
+    Navbar
+}
+from "./Navbar/Navbar";
+export {
+    Sidebar
+}
+from "./Sidebar/Sidebar";
+export {
+    Cards
+}
+from "./Cards/Cards";
